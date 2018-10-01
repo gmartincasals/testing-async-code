999aaa0aa4cde6b473bee348e8e5bbd16d4ae6a9
gerardomartin <gerardomartin@paradigmadigital.com>
2018-10-01 18:12:41 +0200

    Spy and Stub

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,6 +1,13 @@
 module.exports = {
     // A func that takes in two parameters `req` and `res` [request, response]
-    getIndexPage: (req, res) => {
+    getIndexPageSpy: (req, res) => {
         res.send("Hey");
+    },
+    getIndexPageStub: (req, res) => {
+        if (req.user.isLoggedIn()) {
+            return res.send("Hey");
+        }
+        console.log('Estoy pasando por el controller');
+        res.send("Oooops. You need to log in to access this page");
     }
 }
--- tests/controllers/app.controller.test.js
+++ tests/controllers/app.controller.test.js
@@ -1,19 +1,58 @@
 const chai = require("chai");
 const expect = chai.expect;
+
 const sinon = require("sinon");
 
 const indexPage = require("../../controllers/app.controller");
 
-describe("getIndexPage", ()=>{
-    it("Should return index page", ()=>{
-        let req = {}
-        let res = {
-            send: sinon.spy()
-        }
+const user = {
+    addUser: (name) => {
+        this.name = name;
+    }
+}
 
-        indexPage.getIndexPage(req,res);
-        //console.log(res.send);
-        expect(res.send.calledOnce).to.be.true;
-        expect(res.send.firstCall.args[0]).to.equal("bla");
+describe("AppController", ()=> {
+    //Definición variables..
+    let user = {
+        isLoggedIn: ()=>{}
+    };
+    let req = {
+        user: user
+    };
+    let res = {
+        send: sinon.spy()
+    };
+  
+
+    describe("getIndexPageSpies", ()=> {
+        it("should send hey", ()=> {
+        
+
+            indexPage.getIndexPageSpy(req,res);
+            //console.log(res.send);
+            expect(res.send.calledOnce).to.be.true;
+            expect(res.send.firstCall.args[0]).to.equal("Hey");
+            
+
+        });
+    });
+
+    describe("getindexpageStub", ()=>{
+        it("Should send hey when user is lgged in", ()=>{
+            const isLoggedInStub = sinon.stub(user, "isLoggedIn").returns(true);          
+            expect(isLoggedInStub.calledOnce).to.be.true;
+        });
+    });
+});
+
+describe("User", ()=>{
+    describe("addUser", ()=>{
+        it("Should add a user", ()=>{
+            sinon.spy(user, "addUser");
+            user.addUser('John Doe');
+
+            //console.log(user.addUser);
+            expect(user.addUser.calledOnce).to.be.true;
+        });
     });
 });
